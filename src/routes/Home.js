968fecf555e76136864df44ca3ecf597f7a2e201
jaehyun Jeong <wogus0948@naver.com>
2021-08-26 23:32:21 +0900

    finished main description

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,15 +12,34 @@ class Home extends React.Component {
   render() {
     const { is_loading } = this.state;
     return (
-      <section className="container">
+      <section id="container">
         {is_loading ?
         (
           <div className="loader">
             <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div>
-            
+          <div id="main__desc">
+            <div id='title'>
+              <div id="title__sub">SMART WATCH</div>
+              <div id="title__main">This will change your life forever!</div>
+              <div id="title__desc">A smartwatch is a portable device that's designed to be worn on a wrist. Like smartphones, they use touchscreens.</div>
+            </div>
+            <div className="checkbox">
+              <label className="checkbox_container"> 
+                <input type="checkbox" className="circle"></input>
+                A smartwatch is a portable
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <div className="checkbox">
+              <label className="checkbox_container">
+                <input type="checkbox" className="circle"></input>
+                A smartwatch is a portable
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <span id="dollar">$ 250</span>
           </div>
         )}
       </section>
