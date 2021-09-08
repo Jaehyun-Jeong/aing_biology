b735f5d2b7b62ae21ce3e537f678cf4bfb56a754
jaehyun Jeong <wogus0948@naver.com>
2021-09-08 13:58:28 +0900

    re-init2

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import Home from "./routes/Home";
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/express_backend")
-      .then((res) => res.json())
-      .then((data) => setData(data.express));
-  }, []);
-
-  console.log(data);
-  
   return (
     <HashRouter>
       <div id="wrap">
