8ef6e361e581641fe286af216d55403ddf40ec69
hyun <wogus0948@naver.com>
2022-01-24 00:33:29 +0900

    add markdown system

--- src/components/article.js
+++ src/components/article.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Markdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkGfm from 'remark-gfm';
 import './article.css';
+import 'katex/dist/katex.min.css';
 
 function article({ title, content }) {
   return (
     <div id="article">
       <div id="article__title">{title}</div>
-      <div id="article__content">{content}</div>
+      <div id="article__content">
+      <Markdown
+        children={content.replaceAll("\\n", "\n")}
+        remarkPlugins={[remarkMath, remarkGfm]}
+        rehypePlugins={[rehypeKatex]}
+      />
+      </div>
     </div>
   )
 }
