9c392bd53f0ad13d3e92c28077be42f2fdea14e1
hyun <wogus0948@naver.com>
2022-02-22 20:07:03 +0900

    fixed markdown system

--- src/components/article.js
+++ src/components/article.js
@@ -4,6 +4,8 @@ import Markdown from 'react-markdown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkGfm from 'remark-gfm';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './article.css';
 import 'katex/dist/katex.min.css';
 
@@ -17,6 +19,24 @@ function article({ title, created_date, body }) {
         children={body.replaceAll("\\n", "\n")}
         remarkPlugins={[remarkMath, remarkGfm]}
         rehypePlugins={[rehypeKatex]}
+        components={{
+          code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
       />
       </div>
     </div>
